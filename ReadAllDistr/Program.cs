69367ccea0b02fcdf93debea8c6bd09d10602f68
vladpros <vlad_pros@mail.ru>
2019-01-25 14:17:30 +0200

    Now list of direct show after 2s

--- ReadAllDistr/Program.cs
+++ ReadAllDistr/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace ReadAllDistr
@@ -20,11 +21,12 @@ namespace ReadAllDistr
          static void Main(string[] args)
         {
             string dirName = DirName();
-            LinkedList<Data1> Dir = new LinkedList<Data1>();
-            Task task = Task.Factory.StartNew(() => GetAllDir(dirName, Dir));
+            LinkedList<Data1> dir = new LinkedList<Data1>();
+            GetAllDir(dirName, dir);
 
-            Console.ReadLine();
-            CoutLinkedList(Dir);
+            Task.WaitAll();
+            Thread.Sleep(2000);
+            CoutLinkedList(dir,dirName);
             Console.ReadLine();
         }
 
@@ -47,6 +49,7 @@ namespace ReadAllDistr
 
             string[] dirs = Directory.GetDirectories(dirName);
             string[] files = Directory.GetFiles(dirName);
+            Task task = null;
 
             lock (_locker)
             {
@@ -54,10 +57,10 @@ namespace ReadAllDistr
                 {
                     Data1 q;
 
-                    q.name = s;
+                    q.name = $"{s}\\";
                     q.list = new LinkedList<Data1>();
                     dir.Add(q);
-                    Task task = Task.Factory.StartNew(() => GetAllDir(s, q.list));
+                    task = Task.Factory.StartNew(() => GetAllDir(s, q.list));
                 }
 
                 foreach (string s in files)
@@ -72,13 +75,13 @@ namespace ReadAllDistr
 
         }
 
-        static void CoutLinkedList(LinkedList<Data1> list)
+        static void CoutLinkedList(LinkedList<Data1> list, string dirName)
         {
             foreach (Data1 q in list)
             {
-                Console.WriteLine(q.name);
+                Console.WriteLine(q.name.Remove(0,dirName.Length));
                 if(q.list !=null)
-                    CoutLinkedList(q.list);
+                    CoutLinkedList(q.list, dirName);
             }
         }
     }
